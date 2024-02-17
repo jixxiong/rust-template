3d70d9c000849f9d2738932422cec5cdba3dd448
jixxiong <xiongjx751@qq.com>
2024-02-17 10:36:37 +0800

    fix

--- src/test.rs
+++ src/test.rs
@@ -10,10 +10,7 @@ fn main() {
     for _ in 1..=if multiple_tests { cin::<i32>() } else { 1 } {
         solve();
     }
-    match unsafe { &mut COUT } {
-        Some(out) => out.flush().unwrap(),
-        None => panic!(),
-    }
+    unsafe { &mut COUT }.as_mut().unwrap().flush().unwrap();
 }
 
 #[rustfmt::skip]
@@ -21,6 +18,6 @@ fn main() {
 fn cin<T: std::str::FromStr>() -> T { unsafe { loop { if let Some(token) = CIN.pop() { return token.parse().ok().expect("Failed parse"); } let mut input = String::new(); io::stdin().read_line(&mut input).expect("Failed read"); CIN = input.split_whitespace().rev().map(String::from).collect(); } } }
 #[allow(unused)]
 #[macro_export]
-macro_rules! cout { ($($arg:tt)*) => {{ match unsafe { &mut COUT } { Some(out) => out.write_fmt(format_args!($($arg)*)).unwrap(), None => panic!(), } } }; }
+macro_rules! cout { ($($arg:tt)*) => {{ unsafe { &mut COUT }.as_mut().unwrap().write_fmt(format_args!($($arg)*)).unwrap() }}; }
 static mut CIN: Vec<String> = Vec::<String>::new();
 static mut COUT: Option<io::BufWriter<io::Stdout>> = None;
