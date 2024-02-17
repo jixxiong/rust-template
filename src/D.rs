95f5889190f29bcb52f15ceec9cfb836ff31c270
jixxiong <xiongjx751@qq.com>
2024-02-17 09:41:32 +0800

    reformat

--- src/D.rs
+++ src/D.rs
@@ -1,15 +1,5 @@
 use std::io::{self, Write};
 
-#[allow(unused)]
-static mut CIN: Vec<String> = Vec::<String>::new();
-static mut COUT: Option<io::BufWriter<io::Stdout>> = None;
-#[rustfmt::skip]
-#[allow(unused)]
-macro_rules! cout { ($($arg:tt)*) => {{ match unsafe { &mut COUT } { Some(out) => out.write_fmt(format_args!($($arg)*)).unwrap(), None => panic!(), } } }; }
-#[rustfmt::skip]
-#[allow(unused)]
-fn cin<T: std::str::FromStr>() -> T { unsafe { loop { if let Some(token) = CIN.pop() { return token.parse().ok().expect("Failed parse"); } let mut input = String::new(); io::stdin().read_line(&mut input).expect("Failed read"); CIN = input.split_whitespace().rev().map(String::from).collect(); } } }
-
 fn solve() {
     todo!()
 }
@@ -25,3 +15,12 @@ fn main() {
         None => panic!(),
     }
 }
+
+#[rustfmt::skip]
+#[allow(unused)]
+fn cin<T: std::str::FromStr>() -> T { unsafe { loop { if let Some(token) = CIN.pop() { return token.parse().ok().expect("Failed parse"); } let mut input = String::new(); io::stdin().read_line(&mut input).expect("Failed read"); CIN = input.split_whitespace().rev().map(String::from).collect(); } } }
+#[allow(unused)]
+#[macro_export]
+macro_rules! cout { ($($arg:tt)*) => {{ match unsafe { &mut COUT } { Some(out) => out.write_fmt(format_args!($($arg)*)).unwrap(), None => panic!(), } } }; }
+static mut CIN: Vec<String> = Vec::<String>::new();
+static mut COUT: Option<io::BufWriter<io::Stdout>> = None;
